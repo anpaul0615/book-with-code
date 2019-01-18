ef79eceae4f8bbf83452314dbecfc291aad9e3c2
PAUL AN <anpaul0615@gmail.com>
2019-01-19 08:31:51 +0900

    "9장 우리가 사는 시간(times)" 실습
    - 통화단위 비교 테스트케이스 추가
    - 통화 조회함수 정의
    - 통화 멤버변수 정의 + 통화 조회함수를 상위클래스로 이동
    - 곱셈연산 함수에서 총액과 함께 통화단위 전달

--- test-driven-development-by-example/section09/section09.step22.test.js
+++ test-driven-development-by-example/section09/section09.step22.test.js
@@ -1,8 +1,8 @@
 /**
- * Step 21
+ * Step 22
  * 
- * - 테스트 대상코드 수정 (상위클래스 Money 에 하위클래스 Franc 객체 생성 함수 정의)
- * - 테스트 통과 확인
+ * - 새로운 테스트 추가 (통화단위 비교)
+ * - 테스트 실패 확인 (currency 함수 정의되지 않음)
  */
 
 class Money {
@@ -19,8 +19,8 @@ class Money {
   static dollar(amount) {
     return new Dollar(amount);
   }
-  static franc(amount) {  // static 함수로 정의
-    return new Franc(amount);  // 하위클래스 Franc 객체 생성해서 반환
+  static franc(amount) {
+    return new Franc(amount);
   }
 }
 
@@ -36,14 +36,14 @@ class Franc extends Money {
   }
 }
 
-describe.skip('Dollar & Franc Calculation (Step 18)', ()=>{
+describe.skip('Dollar & Franc Calculation (Step 22)', ()=>{
   test('Dollar Multifly + Equal Test', () => {
     const five_dollars = Money.dollar(5);
     expect( five_dollars.multifly(3) ).toEqual(Money.dollar(15));
     expect( five_dollars.multifly(5) ).toEqual(Money.dollar(25));
   });
   test('Franc Multifly + Equal Test', () => {
-    const ten_francs = Money.franc(10);  // 상위클래스 Money 를 통해서 하위클래스 Franc 객체 생성
+    const ten_francs = Money.franc(10);
     expect( ten_francs.multifly(2) ).toEqual(Money.franc(20));
     expect( ten_francs.multifly(8) ).toEqual(Money.franc(80));
   });
@@ -53,5 +53,9 @@ describe.skip('Dollar & Franc Calculation (Step 18)', ()=>{
     expect( (Money.franc(10)).equals(Money.franc(10)) ).toBeTruthy();
     expect( (Money.franc(10)).equals(Money.franc(-10)) ).toBeFalsy();
     expect( (Money.dollar(5)).equals(Money.franc(5)) ).toBeFalsy();
+  });
+  test('Currency Compare Test', ()=>{
+    expect( Money.dollar(1).getCurrency() ).toEqual('USD');
+    expect( Money.franc(1).getCurrency() ).toEqual('CHF');
   });
 });
--- test-driven-development-by-example/section09/section09.step25.test.js
+++ test-driven-development-by-example/section09/section09.step25.test.js
@@ -1,13 +1,14 @@
 /**
- * Step 21
+ * Step 25
  * 
- * - 테스트 대상코드 수정 (상위클래스 Money 에 하위클래스 Franc 객체 생성 함수 정의)
+ * - 테스트 실행코드 수정 (multifly 함수에서 Dollar,Franc 생성자 호출시 통화 멤버변수 함께 전달)
  * - 테스트 통과 확인
  */
 
 class Money {
-  constructor(amount) {
+  constructor(amount,currency) {
     this.amount = amount;
+    this.currency = currency;
   }
   getClassName() {
     return this.__proto__.constructor.name;
@@ -16,34 +17,37 @@ class Money {
     return this.amount == obj.amount
       && this.getClassName() === obj.getClassName();
   }
-  static dollar(amount) {
-    return new Dollar(amount);
+  getCurrency() {
+    return this.currency;
   }
-  static franc(amount) {  // static 함수로 정의
-    return new Franc(amount);  // 하위클래스 Franc 객체 생성해서 반환
+  static dollar(amount) {
+    return new Dollar(amount, 'USD');
+  }
+  static franc(amount) {
+    return new Franc(amount, 'CHF');
   }
 }
 
 class Dollar extends Money {
   multifly(n) {
-    return new Dollar(this.amount * n);
+    return new Dollar(this.amount * n, 'USD');  // 통화단위 함께 전달
   }
 }
 
 class Franc extends Money {
   multifly(n) {
-    return new Franc(this.amount * n);
+    return new Franc(this.amount * n, 'CHF');  // 통화단위 함께 전달
   }
 }
 
-describe.skip('Dollar & Franc Calculation (Step 18)', ()=>{
+describe('Dollar & Franc Calculation (Step 25)', ()=>{
   test('Dollar Multifly + Equal Test', () => {
     const five_dollars = Money.dollar(5);
     expect( five_dollars.multifly(3) ).toEqual(Money.dollar(15));
     expect( five_dollars.multifly(5) ).toEqual(Money.dollar(25));
   });
   test('Franc Multifly + Equal Test', () => {
-    const ten_francs = Money.franc(10);  // 상위클래스 Money 를 통해서 하위클래스 Franc 객체 생성
+    const ten_francs = Money.franc(10);
     expect( ten_francs.multifly(2) ).toEqual(Money.franc(20));
     expect( ten_francs.multifly(8) ).toEqual(Money.franc(80));
   });
@@ -53,5 +57,9 @@ describe.skip('Dollar & Franc Calculation (Step 18)', ()=>{
     expect( (Money.franc(10)).equals(Money.franc(10)) ).toBeTruthy();
     expect( (Money.franc(10)).equals(Money.franc(-10)) ).toBeFalsy();
     expect( (Money.dollar(5)).equals(Money.franc(5)) ).toBeFalsy();
+  });
+  test('Currency Compare Test', ()=>{
+    expect( Money.dollar(1).getCurrency() ).toEqual('USD');
+    expect( Money.franc(1).getCurrency() ).toEqual('CHF');
   });
 });
