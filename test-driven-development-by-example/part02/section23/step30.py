f8219fef05b89298496a1567836a0bd9463aef87
PAUL AN <anpaul0615@gmail.com>
2019-02-02 10:10:42 +0900

    "23장 얼마나 달콤한지" 실습
    - 새로운 테스트케이스 추가 (다중 테스트)
    - 모델코드 수정 (TestSuite 클래스 정의)
    - 모델코드 수정 (TestCase.run, TestSuite.run 에서 호스트코드에서 건내받은 TestResult 객체를 사용하도록 변경)
    - 리팩토링 (TestResult 객체 생성시점을 TestCaseTest.setUp 으로 이동)
    - 리팩토링 (TestCase.assertEqual, TestCase.assertNotEqual 출력포멧 수정)

---
+++ test-driven-development-by-example/part02/section23/step30.py
@@ -0,0 +1,126 @@
+###
+# Step 30
+#
+# - 모델코드 수정 (TestSuite 클래스 정의)
+# - 테스트 실패 확인 (TestSuite.run 에서 TestCase.run 호출할때 파라미터 개수 불일치)
+#
+###
+
+
+class TestCase:
+	def __init__(self, name):
+		self.name = name
+
+	def setUp(self):
+		pass
+	
+	def tearDown(self):
+		pass
+
+	def run(self):
+		result = TestResult()
+		result.testStarted()
+		self.setUp()
+		try:
+			method = getattr(self, self.name)
+			method()
+		# except:
+		# 	result.testFailed()
+		except Exception as e:
+			print(str(e))  ## 디버깅용 로그
+			result.testFailed()
+		self.tearDown()
+		return result
+
+	def assertEqual(self, actual, expected, isReversed=False):
+		assertion = (actual != expected) if isReversed else (actual == expected)
+		if assertion:
+			pass
+		else:
+			print("Assert%sEqual \t : %s" % ("Not" if isReversed else "", assertion))
+			print("- Actual \t : %s" % actual)
+			print("- Expected \t : %s" % expected)
+
+	def assertNotEqual(self, actual, expected):
+		self.assertEqual(actual, expected, True)
+
+
+class TestResult:
+	def __init__(self):
+		self.runCount = 0
+		self.failureCount = 0
+	
+	def testStarted(self):
+		self.runCount += 1
+
+	def testFailed(self):
+		self.failureCount += 1
+
+	def summary(self):
+		return "%d run, %d failed" % (self.runCount, self.failureCount)
+
+
+class WasRun(TestCase):
+	def __init__(self, name):
+		super().__init__(name)
+
+	def setUp(self):
+		self.wasRun = None
+		self.log = "setUp "
+
+	def testMethod(self):
+		self.log = self.log + "testMethod "
+
+	def testBrokenMethod(self):
+		raise Exception
+	
+	def tearDown(self):
+		self.log = self.log + "tearDown "
+
+
+class TestSuite:		## TestSuite 클래스 정의
+	def __init__(self):
+		self.tests = []
+	
+	def add(self,test):
+		self.tests.append(test)
+
+	def run(self):
+		result = TestResult()
+		for test in self.tests:
+			test.run(result)
+		return result
+
+
+class TestCaseTest(TestCase):
+	def setUp(self):
+		self.test = WasRun("testMethod")
+	
+	def testTemplateMethod(self):
+		self.test.run()
+		self.assertEqual(actual=self.test.log, expected="setUp testMethod tearDown ")
+	
+	def testResult(self):
+		test = WasRun("testMethod")
+		result = test.run()
+		self.assertEqual(actual=result.summary(), expected="1 run, 0 failed")
+		
+	def testFailedResult(self):
+		test = WasRun("testBrokenMethod")
+		result = test.run()
+		self.assertEqual(actual=result.summary(), expected="1 run, 1 failed")
+		self.assertNotEqual(actual=result.summary(), expected="1 run, 2 failed")
+
+	def testSuite(self):
+		suite = TestSuite()
+		suite.add(WasRun("testMethod"))
+		suite.add(WasRun("testBrokenMethod"))
+		result = suite.run()
+		self.assertEqual(actual=result.summary(), expected="2 run, 1 failed")
+
+
+
+TestCaseTest("testTemplateMethod").run()
+TestCaseTest("testResult").run()
+TestCaseTest("testFailedResult").run()
+TestCaseTest("testSuite").run()		## err..!
