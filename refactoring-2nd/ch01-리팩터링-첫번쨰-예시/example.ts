1cbface2dd6d66f000f9f1d48775f102010f6835
paulan <anpaul0615@gmail.com>
2022-11-03 23:36:06 +0900

    "1.6. 반복문을 파이프라인으로 바꾸기"

--- refactoring-2nd/ch01-리팩터링-첫번쨰-예시/example.ts
+++ refactoring-2nd/ch01-리팩터링-첫번쨰-예시/example.ts
@@ -124,20 +124,12 @@ function statement(invoice: Invoice, plays: Plays) {
   
   /* inline function */
   function totalVolumeCredits(data: StatementData) { 
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.volumeCredits!;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.volumeCredits!, 0);
   }
 
   /* inline function */
   function totalAmount(data: StatementData) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount!;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.amount!, 0);
   }
 }
 
