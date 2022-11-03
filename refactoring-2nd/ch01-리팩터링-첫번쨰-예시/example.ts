93061c724bd3971f018efb526acd878891210d53
paulan <anpaul0615@gmail.com>
2022-11-03 19:05:18 +0900

    "1.4. 변수 인라인하기"

--- refactoring-2nd/ch01-리팩터링-첫번쨰-예시/example.ts
+++ refactoring-2nd/ch01-리팩터링-첫번쨰-예시/example.ts
@@ -46,10 +46,8 @@ function statement(invoice: Invoice, plays: Plays) {
     totalAmount += amountFor(perf);
   }
 
-  let volumeCredits = totalVolumeCredits();
-
   result += `총액: ${usd(totalAmount)}\n`
-  result += `적립포인트: ${volumeCredits}점\n`
+  result += `적립포인트: ${totalVolumeCredits()}점\n`
 
   return result;
 
